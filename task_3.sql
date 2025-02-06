b0283f8265788a9258adb830a4345ac829841770
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-02-06 04:53:46 -0800

    Create task_3.sql to show all table created

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- to listing all the tables in alx_book_store in MySQL server
+
+SELECT * FROM Authors;
+SELECT * FROM Books;
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+SELECT * FROM Orders_Details;
