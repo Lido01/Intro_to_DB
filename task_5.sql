cbd01e64758564c007695ae74a87549be6d337be
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-02-07 00:28:22 -0800

    Update task_5.sql with syntax correcting

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,4 @@
 -- inserts a single row in the table customer (database alx_book_store) in MySQL server.
-
 USE alx_book_store;
 SELECT * FROM Customers;
-INSERT INTO Customers(customer_id, customer_name, email,address) VALUES("1","Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
+INSERT INTO customers(customer_id, customer_name, email,address) VALUES("1","Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
