38ee8b8c8760148e70afafabf48620d6d4c5ecde
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-02-07 00:30:02 -0800

    Update task_6.sql with correct table name

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- inserts in many row in the table customer (database alx_book_store) as data given in MySQL server.
 USE alx_book_store;
 SELECT * FROM Customers;
-INSERT INTO Customers(customer_id, customer_name, email,address)
-VALUES("2","Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave"),
-      ("3","Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave"),
-      ("4", "Nehemial Kamolu", "nkamolu@santech.com","126 Happiness Ave");
+INSERT INTO customers(customer_id, customer_name, email,address)
+VALUES("2","Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+      ("3","Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+      ("4", "Nehemial Kamolu", "nkamolu@santech.com","126 Happiness Ave.");
