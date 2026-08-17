29713a658819a013bfc93750ff4fdeb3af85ef5a
Lidetu Tesfaye <lidosinafanis@gmail.com>
2026-08-17 19:57:36 +0300

    book table model after auth model sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,3 +7,13 @@ CREATE TABLE Authors (
     author_name VARCHAR(215) NOT NULL
 );
 
+-- Create the Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
