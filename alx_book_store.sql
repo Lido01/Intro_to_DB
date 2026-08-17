2444f64b3cf26860a11160787560332dadf4f78d
Lidetu Tesfaye <lidosinafanis@gmail.com>
2026-08-17 20:01:02 +0300

    iders detail for each oder description

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,7 +23,9 @@ CREATE TABLE Customers (
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT NOT NULL
+
 );
+
 -- Create the Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,3 +33,14 @@ CREATE TABLE Orders (
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
+
+-- Create the Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
